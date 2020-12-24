0aac5f1226c2cce5d6c4e29bceb651cb850ef2f8
Admin <Admin@DESKTOP-5K0QED9>
2020-12-24 00:04:16 -0800

    Add: 3 Jlabels Add: Counter Add: Functions for Actions

--- src/tictactoegui2/SouthPanel.java
+++ src/tictactoegui2/SouthPanel.java
@@ -2,6 +2,12 @@ package tictactoegui2;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -10,9 +16,82 @@ import javax.swing.JPanel;
  */
 public class SouthPanel extends JPanel {
 
+    private JLabel playerX;
+    private JLabel playerO;
+    private JLabel draw;
+    private int xCount;
+    private int oCount;
+    private int drawCount;
+    
     public SouthPanel() {
+        
+        playerX = new JLabel();
+        playerO = new JLabel();
+        draw = new JLabel();
+        
+        xCount = 0;
+        oCount = 0;
+        drawCount = 0;
+        
+        playerX.setText("Player X = " + xCount);
+        playerX.setForeground(Color.WHITE);
+        
+        playerO.setText("Player O = " + oCount);
+        playerO.setForeground(Color.WHITE);
+        
+        draw.setText("Draw = " + drawCount);
+        draw.setForeground(Color.WHITE);
+        
         this.setPreferredSize(new Dimension(600,100));
+        this.setBackground(Color.BLACK);
+        this.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 2));
+        
+        this.setLayout(new GridBagLayout());
+        groupLayout();
+        
     }
     
+    public void groupLayout(){
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(0, 20, 0, 20);
+        this.add(playerX,c);
+        
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0;
+        c.weighty = 0;
+        c.gridx = 1;
+        c.gridy = 0;
+        c.insets = new Insets(0, 20, 0, 20);
+        this.add(playerO,c);
+        
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0;
+        c.weighty = 0;
+        c.gridx = 2;
+        c.gridy = 0;
+        c.insets = new Insets(0, 20, 0, 20);
+        this.add(draw,c);
+    }
+    
+    public void addXCount(int num){
+        xCount += num;
+    }
+    
+    public void addOCount(int num){
+        oCount += num;
+    }
+    
+    public void addDrawCount(int num){
+        drawCount += num;
+    }
+    
+    public void displayCount(){
+        playerX.setText("Player X = " + xCount);
+        playerO.setText("Player O = " + oCount);
+        draw.setText("Draw = " + drawCount);
+    }
     
 }
